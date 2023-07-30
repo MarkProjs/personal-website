5058deacbcf7acb744e34d7872c033a2fff8f2a5
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-07-29 21:37:51 -0400

    adding the underline animation

--- client/src/navigation/NavBar.js
+++ client/src/navigation/NavBar.js
@@ -1,24 +1,25 @@
 import { NavLink } from "react-router-dom";
+import './assets/NavBar.css';
 
 function NavBar() {
   return (
     <div className="NavBar">
-      <NavLink to="/" activestyle="true" className="nav-item">
+      <NavLink to="/" activeClassName="active" className="nav-item">
         HOME
       </NavLink>
-      <NavLink to="/about" activestyle="true" className="nav-item">
+      <NavLink to="/about" activeClassName="active" className="nav-item">
         ABOUT
       </NavLink>
-      <NavLink to="/skills" activestyle="true" className="nav-item">
+      <NavLink to="/skills" activeClassName="active" className="nav-item">
         SKILLS
       </NavLink>
-      <NavLink to="/education" activestyle="true" className="nav-item">
+      <NavLink to="/education" activeClassName="active" className="nav-item">
         EDUCATION
       </NavLink>
-      <NavLink to="/experience" activestyle="true" className="nav-item">
+      <NavLink to="/experience" activeClassName="active" className="nav-item">
         EXPERIENCE
       </NavLink>
-      <NavLink to="/contact" activestyle="true" className="nanav-itemMenu">
+      <NavLink to="/contact" activeClassName="active" className="nav-item">
         CONTACT
       </NavLink>
     </div>
