37062c542b8f988c8f2d6fac75cb6f351340fe4a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-08-29 12:33:31 -0400

    changing the name for home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,9 @@ function Home() {
   return (
     <div className="Home">
       <div className="welcome">
-        <h1 id="title">I am a Junior Software Developer</h1>
+        <h1 id="title">Hello, my name is Mark</h1>
         <p id="briefDesc">
-          Junior Software Developer Recent Computer Science graduate seeking new
+          Recent Computer Science graduate seeking new
           opportunities to utilize coding skills that was taught. Hopeful to
           gain an experience and an entry level position in a business with the
           possibility of obtaining a career in software development.
