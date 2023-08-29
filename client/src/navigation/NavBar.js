e7ff739256b26ab26db4c9cda6386df877842f90
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-08-29 13:51:32 -0400

    adding the colors and css of my personal website

--- client/src/navigation/NavBar.js
+++ client/src/navigation/NavBar.js
@@ -1,21 +1,36 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './assets/NavBar.css';
+import "./assets/NavBar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function NavBar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
-    <div className="NavBar">
-      <NavLink to="/" activeClassName="active" className="nav-item">
-        HOME
-      </NavLink>
-      <NavLink to="/skills" activeClassName="active" className="nav-item">
-        SKILLS
-      </NavLink>
-      <NavLink to="/experience" activeClassName="active" className="nav-item">
-        EXPERIENCE
-      </NavLink>
-      <NavLink to="/contact" activeClassName="active" className="nav-item">
-        CONTACT
-      </NavLink>
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <NavLink to="/" activeClassName="active" className="nav-item">
+          HOME
+        </NavLink>
+        <NavLink to="/skills" activeClassName="active" className="nav-item">
+          SKILLS
+        </NavLink>
+        <NavLink to="/experience" activeClassName="active" className="nav-item">
+          EXPERIENCE
+        </NavLink>
+        <NavLink to="/contact" activeClassName="active" className="nav-item">
+          CONTACT
+        </NavLink>
+      </div>
     </div>
   );
 }
