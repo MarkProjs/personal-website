f392ea9c80d6bcb52167f856e9daa2888a330bf7
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-09-27 19:24:03 -0400

    making a home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,32 @@
-import Intro from './helper-pages/Intro';
+import './styles/Home.css';
 
 function Home() {
   return (
-    <div className="Home">
-      <Intro />
+    <div className="home">
+      <header className="header">
+        <h1>Mark Benedict Agluba</h1>
+        <p>Junior Software Developer</p>
+      </header>
+
+      <section className="hero">
+        <h2>Welcome to My Portfolio!</h2>
+        <p>
+          Recent Computer Science graduate seeking new opportunities to utilize
+          coding skills that was taught. Hopeful to gain an experience and an
+          entry level position in a business with the possibility of obtaining a
+          career in software development.
+        </p>
+      </section>
+
+      <section className='projects'>
+        <h2>
+          My Projects!
+        </h2>
+        <div className='project'>
+          <h3>Project 1</h3>
+          <p>This one :p</p>
+        </div>
+      </section>
     </div>
   );
 }
