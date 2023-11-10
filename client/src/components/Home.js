e5c561153db27e352278a06d70f2ea463246df05
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-11-10 16:40:14 -0500

    adding some stuff for the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,19 @@
 import "./styles/Home.css";
-import image from '../assets/intro-pic.jpg';
+import image from "../assets/intro-pic.jpg";
 
 function Home() {
   return (
     <div className="home">
       <div className="main_heading">
-        <div className="description">
-          <h1> Hello, This is Mark!</h1>
-          <h4>Junior Software Developer</h4>
+        <div className="introduction">
+          <div className="description">
+            <h1> Hello, This is Mark!</h1>
+            <p className="intro">
+              I am a recent computer science graduate with a passion for building web applications. Currently seeking new opportunities as junior developer. Feel free to reach out! 
+            </p>
+          </div>
+          <img src={image} alt="profile_pic" id="profile_pic" />
         </div>
-        <img src={image} alt="profile_pic" id="profile_pic"/>
       </div>
     </div>
   );
