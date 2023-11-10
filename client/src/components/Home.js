393ed60edfd8bbc8ad96cf16155785f4aa615728
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-11-10 16:29:09 -0500

    removing some useless stuff

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,32 +1,16 @@
-import './styles/Home.css';
+import "./styles/Home.css";
+import image from '../assets/intro-pic.jpg';
 
 function Home() {
   return (
     <div className="home">
-      <header className="header">
-        <h1>Mark Benedict Agluba</h1>
-        <p>Junior Software Developer</p>
-      </header>
-
-      <section className="hero">
-        <h2>Welcome to My Portfolio!</h2>
-        <p>
-          Recent Computer Science graduate seeking new opportunities to utilize
-          coding skills that was taught. Hopeful to gain an experience and an
-          entry level position in a business with the possibility of obtaining a
-          career in software development.
-        </p>
-      </section>
-
-      <section className='projects'>
-        <h2>
-          My Projects!
-        </h2>
-        <div className='project'>
-          <h3>Project 1</h3>
-          <p>This one :p</p>
+      <div className="main_heading">
+        <div className="description">
+          <h1> Hello, This is Mark!</h1>
+          <h4>Junior Software Developer</h4>
         </div>
-      </section>
+        <img src={image} alt="profile_pic" id="profile_pic"/>
+      </div>
     </div>
   );
 }
